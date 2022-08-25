63b08e56c80dd0d973c09e46b3f0eea7e81cf31f
roni-mazor <ro6596@gmail.com>
2022-08-25 23:19:19 +0300

    modal class

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,23 +1,32 @@
 const { Link, NavLink } = ReactRouterDOM
-export function AppHeader() {
-
-    function toggleMenu() {
-        
+export class AppHeader extends React.Component {
+    state = {
+        isModalShown: false
     }
 
-    return <header className="full main-layout app-header">
-        <Link className="main-logo" to="/">
-            <i className="icon icon-horse-hopper"></i>
-            <h3>AppSus</h3>
-        </Link>
-        <nav>
-            <NavLink exact to="/">Home</NavLink>
-            <NavLink to="/note">Keep</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/book">Books</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink><img src="../assets/img/books-46.svg" /></NavLink>
-        </nav>
-        <i className="icon icon-grid-view"></i>
-    </header>
+    onToggleModal = () => {
+        this.setState({ isModalShown: !this.state.isModalShown })
+    }
+
+    render() {
+        const modalClass = (this.state.isModalShown) ? 'show' : 'hide'
+
+        return <header className="full main-layout app-header">
+            <Link className="main-logo" to="/">
+                <i className="icon icon-horse-hopper"></i>
+                <h3>AppSus</h3>
+            </Link>
+            <nav className={modalClass}>
+                <NavLink exact to="/">Home</NavLink>
+                <NavLink to="/note">Keep</NavLink>
+                <NavLink to="/mail">Mail</NavLink>
+                <NavLink to="/book">Books</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink><img src="../assets/img/books-46.svg" /></NavLink>
+            </nav>
+            <button onClick={this.onToggleModal}>
+                <i className="icon icon-grid-view"></i>
+            </button>
+        </header>
+    }
 }
