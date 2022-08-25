a15cc3fad9d36a3a1147076234b7c702b6559064
leesharon <leesharooon@gmail.com>
2022-08-25 23:19:58 +0300

    header icons

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -2,7 +2,7 @@ const { Link, NavLink } = ReactRouterDOM
 export function AppHeader() {
 
     function toggleMenu() {
-        
+
     }
 
     return <header className="full main-layout app-header">
@@ -11,12 +11,28 @@ export function AppHeader() {
             <h3>AppSus</h3>
         </Link>
         <nav>
-            <NavLink exact to="/">Home</NavLink>
-            <NavLink to="/note">Keep</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/book">Books</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink><img src="../assets/img/books-46.svg" /></NavLink>
+            <NavLink exact to="/">
+                <img src="../assets/img/home-3.svg" alt="" />
+                <span>Home</span>
+            </NavLink>
+            <NavLink to="/note">
+                <img src="../assets/img/d-edit.svg" alt="" />
+                <span>Keep</span>
+            </NavLink>
+            <NavLink to="/mail">
+                <img src="../assets/img/newsletter.svg" alt="" />
+                <span>Mail</span>
+            </NavLink>
+            <NavLink to="/book">
+                <img src="../assets/img/books-46.svg" alt="" />
+                <span>Books</span>
+            </NavLink>
+            <NavLink to="/about">
+                <img src="../assets/img/question-mark.svg" alt="" />
+                <span>About</span>
+            </NavLink>
+            <NavLink>
+                <img src="../assets/img/books-46.svg" /></NavLink>
         </nav>
         <i className="icon icon-grid-view"></i>
     </header>
