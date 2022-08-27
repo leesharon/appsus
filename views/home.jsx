ab04d879d15cc118462507219ca3aa82da3e20f4
leesharon <leesharooon@gmail.com>
2022-08-27 08:11:00 +0300

    home minor changes

--- views/home.jsx
+++ views/home.jsx
@@ -4,8 +4,8 @@ export function Home() {
     return <section className="home full">
         <div className="hero">
             <div className="slogan-container">
-                <h1>Manage Yourself From a Single Spp</h1>
-                <h1>AppSus</h1>
+                <h1>Manage Yourself From a Single App.</h1>
+                <h1>AppSus.</h1>
             </div>
         </div>
     </section>
