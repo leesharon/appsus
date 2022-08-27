839f86f58af38d15af1fc6391e89059f26cf4a97
leesharon <leesharooon@gmail.com>
2022-08-27 20:26:00 +0300

    fixed icons

--- views/about.jsx
+++ views/about.jsx
@@ -2,7 +2,7 @@
 export function About() {
     return <section className="full main-layout about-page">
         <h1>About AppSus</h1>
-        <img src="../assets/img/question-mark-big.svg" alt="" />
+        <i class="fa-solid fa-question"></i>
         <h2>Who are we?</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel error deleniti nemo delectus, quidem eos quaerat ipsam sit provident inventore possimus perspiciatis ex quod minus voluptate, dolore soluta optio praesentium.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel error deleniti nemo delectus, quidem eos quaerat ipsam sit provident inventore possimus perspiciatis ex quod minus voluptate, dolore soluta optio praesentium.</p>
