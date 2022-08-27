0ca66cc2128073f5b8c60d9e3249483d546a5429
leesharon <leesharooon@gmail.com>
2022-08-27 12:59:03 +0300

    svg fix attempt

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -19,23 +19,23 @@ export class AppHeader extends React.Component {
                 </Link>
                 <nav className={`${modalClass} animate__animated animate__fadeInTopRight `}>
                     <NavLink className="nav-link" onClick={this.onToggleModal} exact to="/">
-                        <img src="../assets/img/home-3.svg" alt="" />
+                        <img src="./assets/img/home-3.svg" alt="" />
                         <span>Home</span>
                     </NavLink>
                     <NavLink className="nav-link" onClick={this.onToggleModal} to="/note">
-                        <img src="../assets/img/d-edit.svg" alt="" />
+                        <img src="./assets/img/d-edit.svg" alt="" />
                         <span>Keep</span>
                     </NavLink>
                     <NavLink className="nav-link" onClick={this.onToggleModal} to="/mail/inbox">
-                        <img src="../assets/img/newsletter.svg" alt="" />
+                        <img src="./assets/img/newsletter.svg" alt="" />
                         <span>Mail</span>
                     </NavLink>
                     <NavLink className="nav-link" onClick={this.onToggleModal} to="/book">
-                        <img src="../assets/img/books-46.svg" alt="" />
+                        <img src="./assets/img/books-46.svg" alt="" />
                         <span>Books</span>
                     </NavLink>
                     <NavLink className="nav-link about" onClick={this.onToggleModal} to="/about">
-                        <img src="../assets/img/question-mark.svg" alt="" />
+                        <img src="./assets/img/question-mark.svg" alt="" />
                         <span>About</span>
                     </NavLink>
                 </nav>
